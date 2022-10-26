c7eb4484d20dbc3c5f36acd49c31b76846eb9058
jakubkosciolek <kuba@piwniczna.com>
2022-10-26 12:12:20 +0200

    Added minimax algorithm - NOT WORKING!

--- game_single.py
+++ game_single.py
@@ -1,4 +1,5 @@
 from game_multi import game_multi
+import numpy as np
 
 """
 I'm using minimax algorithm to define computer moves in a singleplayer mode. This algorithm is minimizing the possible 
@@ -46,3 +47,81 @@ class game_single(game_multi):
 
         # none of players won
         return 0
+
+    def possibleMoves(self):
+        moves = []
+        for row in range(3):
+            for column in range(3):
+                if self.fields[row][column] == ' ':
+                    moves.append(self.fields[row][column])
+        return moves
+
+    # finding the best possible move for the player
+    def bestMove(self):
+        bestScore = -np.inf
+        bestMove = (-1, -1)
+
+        for row in range(3):
+            for column in range(3):
+
+                if self.fields[row][column] == ' ':
+                    self.fields[row][column] = 'o'
+                    moveScore = self.minimax(0, False)
+
+                    # undo the move
+                    self.fields[row][column] == ' '
+
+                    if moveScore > bestScore:
+                        bestMove = (row, column)
+                        bestScore = moveScore
+        print(f"The value of the best move is: {bestScore}")
+        return bestMove
+
+    def minimax(self, depth, isMax):
+        score = self.points()
+
+        # maximizer == 10, minimizer == -10 won the game
+        if score == 10 or score == -10:
+            return score
+
+        # tie
+        if not self.isMoveAvailable():
+            return 0
+
+        # minimizer - computer move
+        best = 1000
+
+        for row in range(3):
+            for column in range(3):
+                if self.fields[row][column] == ' ':
+                    self.fields[row][column] = 'o'
+
+                    # call minimax recursively and choose the minimum value
+                    best = min(best, self.minimax(depth + 1, not isMax))
+
+                    # undo the move
+                    self.fields[row][column] = ' '
+        return best
+
+    def oneRound(self):
+        if self.isMoveAvailable():
+            print(f"Player {1}")
+            self.move("x")
+            if self.ifWin("x"):
+                return
+        else:
+            return
+
+        if self.isMoveAvailable():
+            print(f"Player {2}")
+            self.aiMove()
+            if self.ifWin("o"):
+                return
+        else:
+            return
+
+    def aiMove(self):
+        bestMove = self.bestMove()
+        print("The optimal move is: ")
+        print(f"ROW: {bestMove[0]} COL: {bestMove[1]}")
+        self.fields[bestMove[0]][bestMove[1]]
